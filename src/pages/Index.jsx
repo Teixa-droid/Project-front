d5f32c6e395bc42df3c5fa384b387bc7763a104c
Teixa-droid <ruben.a.teixa@gmail.com>
2022-01-22 15:01:21 +0000

    add workflow

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Index() {
   return (
     <div>
-      <div className='bg-green-300 h-96'>ola</div>
+      <div className='bg-green-300 h-96'>ola deploy</div>
       <div className='bg-green-300 h-96'>Index page</div>
       <div className='bg-green-300 h-96' />
       <div className='bg-green-300 h-96' />
